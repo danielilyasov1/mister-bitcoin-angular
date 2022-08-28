1cc5e1a12298598b9a8363bd1eba9149132e7cc3
danielleilyasov <danielle.ilyas@gmail.com>
2022-08-28 17:26:13 +0300

    new start ,add favicon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
+import { GoogleChartsModule } from 'angular-google-charts';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app-root/app.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { ChartComponent } from './pages/chart/chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent,
-    ChartComponent,
   ],
   imports: [
     BrowserModule,
